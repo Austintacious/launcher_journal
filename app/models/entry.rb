9101f97eea3acdf4463840120346db121319ca5b
Austin Winslow <awinslow21@gmail.com>
2013-12-22 19:17:55 -0500

    add #index, #new, #show

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,8 @@
 class Entry < ActiveRecord::Base
   validates_presence_of :title, :description
   has_many :categories, through: :categorizations
+
+  def self.category_list
+    ["This", "That", "Another", "Blah"]
+  end
 end
